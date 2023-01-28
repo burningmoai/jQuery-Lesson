8ae5cfd8244a0763872750e2e547898ecf91022f
burningmoai <114802516+burningmoai@users.noreply.github.com>
2023-01-28 15:21:12 +0000

    [Add]演習問題

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ $(function () { //短縮系
 //   // bodyタグ内のHTMLを<h1>Hello jQuery!!</h1>で書き換えてね
 //   // $(セレクタ).メソッド(パラメータ);
 // });
-  $('.box1').slideUp()
+  $('.box1').slideDown(10000,function () {
+    $('.box1').css({
+      'background-color':'#0000FF',
+      'width':'200px',
+      'height':'100px'
+    }).slideUp(1000); //青
+  });
 });
